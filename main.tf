d9edaa26e44e0008541aa8a42e26d7ebf4a1a6bb
aravind <aravind.ark@gmail.com>
2024-10-20 19:44:55 +0530

    ingress and egress added

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"   # arguments
+  region = "us-east-1" # arguments
 }
 
 # resource block 
 # vpc
 resource "aws_vpc" "vpc-tf" {
-  cidr_block       = "10.0.0.0/16"
- enable_dns_hostnames = "true"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = "true"
 
   tags = {
     Name = "vpc-tf"
@@ -25,8 +25,8 @@ resource "aws_internet_gateway" "ig-tf" {
 
 # public subnet
 resource "aws_subnet" "pub-subnet-tf" {
-  vpc_id     = aws_vpc.vpc-tf.id   # attach it to the vpc
-  cidr_block = "10.0.1.0/24"    # cidr range
+  vpc_id     = aws_vpc.vpc-tf.id # attach it to the vpc
+  cidr_block = "10.0.1.0/24"     # cidr range
 
   tags = {
     Name = "pub-subnet-tf"
@@ -36,11 +36,11 @@ resource "aws_subnet" "pub-subnet-tf" {
 # route table
 
 resource "aws_route_table" "pub-rt-tf" {
-  vpc_id = aws_vpc.example.id
+  vpc_id = aws_vpc.vpc-tf.id    # mention the vpc here
 
   route {
-    cidr_block = "0.0.0.0/0"       # change to 0.0.0.0 for internet access
-    gateway_id = aws_internet_gateway.ig-tf.id   # attach it to the internet gateway
+    cidr_block = "0.0.0.0/0"                   # change to 0.0.0.0 for internet access
+    gateway_id = aws_internet_gateway.ig-tf.id # attach it to the internet gateway
   }
 
   tags = {
@@ -49,3 +49,39 @@ resource "aws_route_table" "pub-rt-tf" {
 
 }
 
+# route table association
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.pub-subnet-tf.id
+  route_table_id = aws_route_table.pub-rt-tf.id
+}
+
+# sec groups
+
+resource "aws_security_group" "example_sg" {
+  name        = "example_security_group"
+  description = "Security group for example application"
+  vpc_id      = aws_vpc.vpc-tf.id # mention the vpc 
+
+  # Ingress 
+  ingress {
+    from_port   = 80 # 80 (HTTP) - allow on port 80 and similar for 443 
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # from any where you can able to access port:80
+  }
+
+  ingress {
+    from_port   = 22 # Allow 22 (SSH)
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # ssh from anywhere
+  }
+
+  # Egress 
+  egress {
+    from_port   = 0 # Allow all outbound traffic 
+    to_port     = 0
+    protocol    = "-1"          # -1 means all protocols
+    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic to anywhere
+  }
+}
